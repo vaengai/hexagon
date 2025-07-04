9e4e4457fb6d00691322aabc7218ccc3eaf4d2b4
vaengai <venkatesh.jayapal@gmail.com>
2025-07-04 13:52:53 -0500

    Adding the hero banner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 export default function App() {
   return (
     <div className="w-full h-full absolute bg-gradient-to-b from-neutral-950 to-stone-900">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,48 @@
+export default function Hero() {
+  return (
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+        />
+      </div>
+      <div className="mx-auto max-w-6xl py-32 sm:py-48 lg:py-56">
+        <div className="text-center">
+          <h1 className="text-5xl font-extralight tracking-tight text-balance text-sky-400 sm:text-7xl">
+            Your journey to discipline starts here...
+          </h1>
+          <p className="mt-8 text-lg font-light text-pretty text-white sm:text-xl/8">
+            Stay consistent, track your progress, and become the best version of yourself.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <button
+              type="button"
+              className="rounded-lg bg-sky-100 px-8 py-4 text-lg font-semibold shadow-xs hover:bg-sky-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-200"
+            >
+              Add Habit
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+        />
+      </div>
+    </div>
+  );
+}
