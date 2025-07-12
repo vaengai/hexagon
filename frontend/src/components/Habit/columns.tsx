2519389108ee8697884487d2e31da2263501f1ab
vaengai <venkatesh.jayapal@gmail.com>
2025-07-11 21:27:17 -0500

    Updated the status

--- frontend/src/components/Habit/columns.tsx
+++ frontend/src/components/Habit/columns.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import type { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/components/ui/badge";
+import { IconCircleCheckFilled, IconLoader } from "@tabler/icons-react";
 
 export type Habit = {
   id: string;
@@ -17,6 +19,16 @@ export const columns: ColumnDef<Habit>[] = [
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => (
+      <Badge variant="outline" className="text-muted-foreground px-1.5">
+        {row.original.status === "done" ? (
+          <IconCircleCheckFilled className="fill-green-500 dark:fill-green-400" />
+        ) : (
+          <IconLoader />
+        )}
+        {row.original.status}
+      </Badge>
+    ),
   },
   {
     accessorKey: "category",
