f77e505cce2e5337558b286508c552caa4f0a0b9
vaengai <venkatesh.jayapal@gmail.com>
2025-07-24 23:09:07 -0500

    updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path"
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  },
+  base: "/",
 });
